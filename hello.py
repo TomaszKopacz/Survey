aa5748120a0d16fe6aa75ff82f69060612774157
TomaszKopacz <tomasz.kopacz@onet.pl>
2019-04-27 00:04:10 +0200

    Database ready

--- hello.py
+++ hello.py
@@ -17,20 +17,47 @@ class Answer(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime, default=datetime.now)
-    email = db.Column(db.String(20))
-    name = db.Column(db.String(20))
-    faculty = db.Column(db.String(20))
-    is_drunk = db.Column(db.Integer)
-    how_often_drunk = db.Column(db.Integer)
-    planned_drunk = db.Column(db.Integer)
+    season = db.Column(db.String(10))
+    place = db.Column(db.String(10))
+    favourite = db.Column(db.String(50))
+    accommodation = db.Column(db.String(20))
+    mean = db.Column(db.String(15))
+    planning = db.Column(db.String(50))
+    partners = db.Column(db.String(50))
+    is_active = db.Column(db.String(50))
+    activity = db.Column(db.String(50))
+    last_holidays = db.Column(db.String(50))
+    abroad = db.Column(db.String(50))
+    arrangements = db.Column(db.String(100))
+    expenses = db.Column(db.String(30))
 
-    def __init__(self, email, name, faculty, is_drunk, how_often_drunk, planned_drunk):
-        self.email = email
-        self.name = name
-        self.faculty = faculty
-        self.is_drunk = is_drunk
-        self.how_often_drunk = how_often_drunk
-        self.planned_drunk = planned_drunk
+    def __init__(self,
+                 season,
+                 place,
+                 favourite,
+                 accommodation,
+                 mean,
+                 planning,
+                 partners,
+                 is_active,
+                 activity,
+                 last_holidays,
+                 abroad,
+                 arrangements,
+                 expenses):
+        self.season = season
+        self.place = place
+        self.favourite = favourite
+        self.accommodation = accommodation
+        self.mean = mean
+        self.planning = planning
+        self.partners = partners
+        self.is_active = is_active
+        self.activity = activity
+        self.last_holidays = last_holidays
+        self.abroad = abroad
+        self.arrangements = arrangements
+        self.expenses = expenses
 
 
 @app.route("/")
@@ -45,18 +72,55 @@ def show_form():
 
 @app.route("/save", methods=['POST'])
 def save():
-    email = request.form["email"]
-    name = request.form["name"]
-    faculty = request.form["faculty"]
-    is_drunk = request.form["is_drunk"]
-    how_often_drunk = request.form["how_often_drunk"]
-    planned_drunk = request.form["planned_drunk"]
+    _season = request.form["season"]
+    _place = request.form["place"]
+    _favourite = request.form["favourite"]
+    _favourite_text = request.form["favourite_text"]
+    _accommodation = request.form["accommodation"]
+    _mean = request.form["mean"]
+    _planning = request.form["planning"]
+    _partners = request.form["partners"]
+    _partners_text = request.form["partners_text"]
+    _is_active = request.form["is_active"]
+    _activity = request.form["activity"]
+    _last_holidays = request.form["last_holidays"]
+    _abroad = request.form["abroad"]
+    _arrangements = request.form["arrangements"]
+    _expenses = request.form["expenses"]
 
-    answer = Answer(email, name, faculty, is_drunk, how_often_drunk, planned_drunk)
+    answer = Answer(_season,
+                    _place,
+                    get_favourite(_favourite, _favourite_text),
+                    _accommodation,
+                    _mean,
+                    _planning,
+                    get_partners(_partners, _partners_text),
+                    _is_active,
+                    _activity,
+                    _last_holidays,
+                    _abroad,
+                    _arrangements,
+                    _expenses)
     db.session.add(answer)
     db.session.commit()
 
     return redirect("/results/")
+
+
+def get_favourite(_favourite, _favourite_text):
+    if _favourite == "Tak":
+        return _favourite_text
+
+    else:
+        return _favourite
+
+
+def get_partners(_partners, _partners_text):
+    if _partners == "Inne":
+        return _partners_text
+
+    else:
+        return _partners
 
 
 @app.route("/results/")
